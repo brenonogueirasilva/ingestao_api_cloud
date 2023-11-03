8ad915ac8f7b2a6b019b4bb4e71e2271f907ca84
brenonogueirasilva <brenogueirasilva@gmail.com>
2023-11-02 21:11:42 -0300

    <feat> : create variables.tf file

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "project_id" {
+  type = string
+  default = "apt-theme-402300"
+}
+
+variable "region" {
+  type = string
+  default = "us-central1"
+}
+
+variable "service_account_email" {
+  type = string
+  default = "brasil-api-cloud-storage@apt-theme-402300.iam.gserviceaccount.com"
+  
+}
